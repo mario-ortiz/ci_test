f1e37ba1eb2806a5b81a35597e184b17f72c85e3
Mario Sevilla <rogelio.sevilla@gmail.com>
2018-04-20 14:38:09 -0500

    test multiple deploy

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,6 +4,6 @@ module.exports = (server) => {
     const HOME_ROUTE = '/';
     // Get a list of users
     server.get(HOME_ROUTE, (req, res) => {
-        return res.send('At Home Staging');
+        return res.send('At Home Multiple Deploys');
     });
 }
