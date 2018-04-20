7b095179d008fd19469ce266cb5336940a01a91c
Mario Sevilla <rogelio.sevilla@gmail.com>
2018-04-20 16:14:41 -0500

    Add new .ebextensions to check health

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,7 +3,7 @@ const Users = require('./../models/users');
 module.exports = (server) => {
     // Get a list of users
     server.get('/', (req, res) => {
-        return res.send('At Home Multiple Deployments');
+        return res.send('At Home Multiple Deployments and health check');
     });
 
     server.get('/health', (req, res) => {
